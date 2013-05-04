4c3612a571712a437e607c594f2bb307d32c8e7f
Andrey Koleshko <ka8725@gmail.com>
2013-05-04 23:58:05 +0300

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrkonliner::Application.config.secret_token = '8dd2667b522b00c7ffd639219f4f6fc18c182afaacb3d32a8c2b319b857a39ff9c13cbb1a00d81dce89eeec3296b3574f7bc197037662334076332d70bd8108b'
